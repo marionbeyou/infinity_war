85a0caa0682debe5e613646dbc8157773727e383
votrepseudo <jk.jakubowski25@gmail.com>
2018-04-09 18:22:59 +0200

    Debuts
    Initialization

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// var marvel = 
+// {   
+//     const url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=ddf7eaea84ecd9b6d4249fd73cf53332&hash=d1631fc0247b6108cfc1d0bd66dc961d"
+//     let render = () =>
+//     {
+//         let message = document.getElementsByClassName(message);
+//     }
+// };
+
+
